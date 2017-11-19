d310884e86190157799c85bc2ad1d808bc7beb8f
Torok Mark <torok.mark@gmx.com>
2017-11-19 23:23:51 +0100

    Dev malloy

--- demo.js
+++ demo.js
@@ -1,8 +1,29 @@
 
 var malloy = require('malloy'),
-    configmalloy = require('malloy').adopt({});
+    configmalloy = require('malloy').adopt({location: __filename, park: 'yellowstone', storage: '/home/mint/log.out', dateformat: 'isoTime'});
 
-console.log(malloy);
 
-malloy.says("hello", {level : 'Woody', filename: __filename });
-malloy.toWoody("hola", {})
+malloy.says('hello');
+malloy.says();
+malloy.says('data go to', {location: 'file...'});
+malloy.says('hello', {to: 'denzEl'});
+malloy.says('just in the air', {to: 'ddddenzEl'});
+malloy.says('something in', {park: 'yellowstone'});
+malloy.says('something else in', {park: 'brickleberry'});
+
+malloy.saysToWoody('hello');
+malloy.saysToDenzel();
+malloy.saysToEthel('here the lipstick', {location: 'file...'});
+malloy.saysToSteve('ohh it is severe!', {park: 'brickleberry'});
+malloy.toDenzel('an animal like bug is over there', {animal: 'bug'} );
+malloy.saysToDenzel('an animal like deer is not shown', {animal: 'deer'} );
+malloy.toEthel('In Yellowstone', {park: 'yellowstone'});
+malloy.toSteve('In Brickleberry', {park: 'brickleberry', storage: '/home/mint/log.out'});
+
+configmalloy.toEthel('hhhh', {location: '....', dateformat: 'isoDate'});
+configmalloy.toDenzel('dennzzz', {animal: 'bug'});
+configmalloy.toDenzel('dennzzz in brick', {animal: 'bug', park: 'brick'});
+configmalloy.toSteve('hola stevie!');
+
+
+
--- malloy.js
+++ malloy.js
@@ -10,50 +10,108 @@ var path = require('path'),
 
 var Malloy = function Malloy(config) {
   var that = this;
-  if (config.filepath === undefined || config.filepath === null || fs.existSync(path.dirname(config.filepath)) ) {
 
-  } else {
-    throw new Error("filepath could not be found")
+  that.defaults = config || {};
+
+  that.defaults.dateformat = that.defaults.dateformat || 'isoDateTime';
+  that.defaults.park = that.defaults.park === 'yellowstone' ? 'yellowstone' : 'brickleberry';
+  that.defaults.location = that.defaults.location || '';
+  that.defaults.storage = that.defaults.storage || 'STDOUT';
+
+  getDialect = function(to, park) {
+    var level = {
+      'DENZEL' : 'DEBUG',
+      'WOODY' : 'WARNING',
+      'MALLOY' : 'INFO',
+      'ETHEL' : 'ERROR',
+      'STEVE' : 'SEVERE'
+    }
+    if (park === 'brickleberry') {
+      return to
+    } else {
+      return level[to];
+    }
   }
-  // file : __filename
-  // style: brickleberry vs formal
-  // dateformat
+
+  that.buildFullMsg = function(msg, to, config) {
+    var config = config || {},
+        log = '| ',
+        to = to.toUpperCase(),
+        dateformat = config.dateformat || that.defaults.dateformat,
+        location = config.location || that.defaults.location,
+        park = config.park || that.defaults.park;
+
+    switch (to) {
+      case 'DENZEL':
+        log += colors.magenta(getDialect(to, park)); break;
+      case 'WOODY':
+        log += colors.yellow(getDialect(to, park)); break;
+      case 'ETHEL':
+        log += colors.red(getDialect(to, park)); break;
+      case 'STEVE':
+        log += colors.black.bgRed(getDialect(to, park)); break;       
+      default:
+        log += colors.white(getDialect('MALLOY', park));
+    }
+    
+    log += ' | ' + dateFormat(Date.now(), dateformat); 
+    log += ' | ' + location;
+    log += ' >> ' + msg;
+    return log;
+  }
+
+  that.log = function(msg, to, config) {
+    var file = (config && config.storage) || that.defaults.storage,
+        fullMsg = that.buildFullMsg(msg, to, config);
+
+    if (file !== 'STDOUT' && fs.existsSync(path.dirname(file))) {
+      let stream = fs.createWriteStream(file, {'flags' : 'a'});
+      stream.once('open', function(fd) {
+        stream.write(fullMsg + '\n');
+        stream.end();
+      });
+    } else {
+      console.log(fullMsg);       
+    }
+  }
 }
 
 Malloy.prototype.says = function(msg, config) {
-  console.log('| ' + colors.yellow(config.level) + ' | ' + dateFormat(Date.now(), 'isoDateTime') + ' | ' + config.filename + ' >> ' + msg);
+    var config = config || {};
+    this.log(msg, config.to || 'malloy', config);
 }
 
 
 Malloy.prototype.saysToDenzel = function(msg, config) {
-  config.level = 'Denzel';
-  this.says(msg, config);
+  if (config && config.animal === 'bug') {
+    this.log(msg, 'denzel', config);
+  }
 }
 Malloy.prototype.toDenzel = Malloy.prototype.saysToDenzel;
 
 
 Malloy.prototype.saysToWoody = function(msg, config) {
-  config.level = 'Woody';
-  this.says(msg, config);
+  this.log(msg, 'woody', config);
 }
 Malloy.prototype.toWoody = Malloy.prototype.saysToWoody;
 
 
 Malloy.prototype.saysToEthel = function(msg, config) {
-  config.level = 'Ethel';
-  this.says(msg, config);
+  this.log(msg, 'ethel', config);
 }
 Malloy.prototype.toEthel = Malloy.prototype.saysToEthel;
 
 
 Malloy.prototype.saysToSteve = function(msg, config) {
-  config.level = 'Steve';
-  this.says(msg, config);
+  this.log(msg, 'steve', config);
 }
 Malloy.prototype.toSteve = Malloy.prototype.saysToSteve
 
 
 module.exports = new Malloy();
 module.exports.adopt = function(config) {
+  if (config === undefined || config === null || config === {}) {
+    throw new Error('configuration could not be found');
+  }
   return new Malloy(config);
 }
